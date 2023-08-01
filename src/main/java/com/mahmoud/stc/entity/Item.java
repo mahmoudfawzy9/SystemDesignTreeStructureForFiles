0d7d10de4a0449d2e98a151cf1202145e3088d65
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-01 23:12:07 +0300

    Update Item.java

--- src/main/java/com/mahmoud/stc/entity/Item.java
+++ src/main/java/com/mahmoud/stc/entity/Item.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.mahmoud.stc.enums.ItemType;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.List;
 
 
