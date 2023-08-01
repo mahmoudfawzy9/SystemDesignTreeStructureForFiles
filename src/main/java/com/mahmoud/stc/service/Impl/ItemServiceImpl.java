40c8f0bf58d345d2ec73adb9453d7dd8dc0f408c
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-01 23:16:26 +0300

    Update ItemServiceImpl.java

--- src/main/java/com/mahmoud/stc/service/Impl/ItemServiceImpl.java
+++ src/main/java/com/mahmoud/stc/service/Impl/ItemServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
