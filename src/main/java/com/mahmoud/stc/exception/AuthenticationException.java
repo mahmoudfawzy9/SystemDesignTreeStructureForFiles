be3d6bb1b0c7bbfc53df9fcc2d5dbdf36fed9988
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-04 18:02:13 +0300

    Create AuthenticationException.java

---
+++ src/main/java/com/mahmoud/stc/exception/AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.mahmoud.stc.exception;
+
+public class AuthenticationException extends RuntimeException {
+  
+    public AuthenticationException(String s) {
+        super(s);
+    }
+}
