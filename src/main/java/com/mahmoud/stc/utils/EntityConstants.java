3da5e7bf4f7a53f2bdb9b1aa252a9d7bb3d3d17d
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-04 18:04:25 +0300

    Create EntityConstants.java

---
+++ src/main/java/com/mahmoud/stc/utils/EntityConstants.java
@@ -0,0 +1,8 @@
+package com.mahmoud.stc.utils;
+
+public final class EntityConstants {
+
+    //Number of seconds that login token stay valid during
+    public static final int AUTH_TOKEN_VALIDITY = 2592000; // 1 month
+  
+}
