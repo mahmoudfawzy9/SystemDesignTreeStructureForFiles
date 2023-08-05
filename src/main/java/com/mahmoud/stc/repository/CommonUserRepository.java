c7a8455f2a626ae3608fd019a540c4bb9951179c
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-06 00:29:28 +0300

    Update CommonUserRepository.java

--- src/main/java/com/mahmoud/stc/repository/CommonUserRepository.java
+++ src/main/java/com/mahmoud/stc/repository/CommonUserRepository.java
@@ -1,10 +1,18 @@
 package com.mahmoud.stc.repository;
 
 import com.mahmoud.stc.entity.BaseUserEntity;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface CommonUserRepository{
   
     List<String> getUserRoles(BaseUserEntity user);
-  
+
+
+    BaseUserEntity saveAndFlush(BaseUserEntity userEntity);
+
+    Optional<BaseUserEntity> findById(Long id, Boolean isAdmin);
+
+    Optional<BaseUserEntity> findByUserName(String username);
 }
