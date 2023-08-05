297b506736106f1345dc4b4c774188033a46b995
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-06 00:27:29 +0300

    Update SecurityService.java

--- src/main/java/com/mahmoud/stc/service/SecurityService.java
+++ src/main/java/com/mahmoud/stc/service/SecurityService.java
@@ -3,16 +3,26 @@ package com.mahmoud.stc.service;
 import com.mahmoud.stc.config.UserAuthenticationData;
 import com.mahmoud.stc.entity.BaseUserEntity;
 import com.mahmoud.stc.entity.UserEntity;
+import com.mahmoud.stc.entity.UserToken;
 import com.mahmoud.stc.enums.Role;
+import org.springframework.cache.annotation.CacheEvict;
 
 import java.util.Optional;
 import java.util.Set;
+
+import static com.mahmoud.stc.cache.Caches.USERS_BY_TOKENS;
 
 public interface SecurityService {
 
     Optional<UserAuthenticationData> findUserDetailsByAuthToken(String token);
 
+    @CacheEvict(cacheNames = {USERS_BY_TOKENS}, key = "#userToken.token")
+    UserToken extendUserExpirationTokenIfNeeded(UserToken userToken);
+
+
     BaseUserEntity getCurrentUser();
+
+    Optional<BaseUserEntity> getCurrentUserOptional();
 
     Long getCurrentUserFileId(UserEntity userId);
 
@@ -21,8 +31,6 @@ public interface SecurityService {
     Boolean userHasRole(Role role);
   
     Boolean currentUserHasRole(Role role);
-
-    Optional<BaseUserEntity> getCurrentUserOptional();
 
     boolean currentUserHasMaxRoleLevelOf(Role role);
 
