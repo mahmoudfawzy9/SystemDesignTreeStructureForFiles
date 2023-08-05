cd4604d23165781c4c09d6dbc61fc982760cb5ec
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-08-06 02:23:07 +0300

    Update UserEntity.java

--- src/main/java/com/mahmoud/stc/entity/UserEntity.java
+++ src/main/java/com/mahmoud/stc/entity/UserEntity.java
@@ -5,9 +5,6 @@ import com.mahmoud.stc.enums.Role;
 import com.mahmoud.stc.enums.UserStatus;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -20,7 +17,7 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @EqualsAndHashCode(callSuper = false)
-public class UserEntity extends BaseUserEntity implements UserDetails {
+public class UserEntity extends BaseUserEntity {
 
 
     @Column(name = "user_name")
@@ -38,54 +35,9 @@ public class UserEntity extends BaseUserEntity implements UserDetails {
 
     @Column(name = "mobile")
     private String mobile;
+    
     @Column(name = "date_of_birth")
     private LocalDateTime dateOfBirth;
 
 
-    @Override
-    public List<GrantedAuthority> getAuthorities(){
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role.toString())));
-        return authorities;
-    }
-
-
-    @Override
-    public String getPassword() {
-        return getEncryptedPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public UserRepresentationObject getRepresentation() {
-        UserRepresentationObject obj = new UserRepresentationObject();
-        obj.setId(getId());
-        obj.setStatus(UserStatus.getUserStatus(getUserStatus()).name());
-        obj.setImage(this.getAvatar());
-        return obj;
-    }
 }
